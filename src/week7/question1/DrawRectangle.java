122ef6b0d7051587951a7a628b1882a88285d92a
Stephenlao <laovinhkhang2002@gmail.com>
2023-06-30 16:58:15 +0700

    update question week7 question 1

---
+++ src/week7/question1/DrawRectangle.java
@@ -0,0 +1,17 @@
+package week7.question1;
+import java.util.Scanner;
+public class DrawRectangle {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        int width = scanner.nextInt();
+        System.out.println("Enter length: ");
+        int length = scanner.nextInt();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
